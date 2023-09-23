6a61d128b9bf0b4d12ba8155c0c6dbb1b20f9f76
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2023-09-23 00:38:21 +0000

    inclui2dados

--- JogoDado/App.js
+++ JogoDado/App.js
@@ -1,32 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
+const lados=[
+  require('./assets/imag/1a.jpg'),
+  require('./assets/imag/2a.jpg'),
+  require('./assets/imag/3a.jpg'),
+  require('./assets/imag/4a.jpg'),
+  require('./assets/imag/5a.jpg'),
+  require('./assets/imag/6a.jpg'),
+]
+
 
 export default function App() {
+  const [iLados, setLados ] = useState(0)
+  const [iLados2, setLados2 ] = useState(2)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Jogo do Dado!</Text>
+      <Image source={lados[iLados]} />
+      <Image source={lados[iLados2]} />
+      <Button title='Jogar' 
+      onPress={() =>  { 
+        setLados(randomLados())
+        setLados2(randomLados())}
+      }
+      />
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
-export default function App(){
-  const lados=[
-      require('./1a'),
-      require('./1b'),
-      require('./2a'),
-      require('./2b'),
-      require('./3a'),
-      require('./3b'),
-      require('./4a'),
-      require('./4b'),
-      require('./5a'),
-      require('./5b'),
-      require('./6a'),
-      require('./6b'),
-  ]
-let iLados=0;
-}
+const randomLados = () => {
+  return Math.floor((Math.random() * 6));
+};
+
+
 
 const styles = StyleSheet.create({
   container: {
